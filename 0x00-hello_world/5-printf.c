6540b9247c2ca9eb551fc77215b49107c952c0f2
JacobM311 <jacobmarshall9608@yahoo.com>
2022-05-20 11:40:21 -0700

    commit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,6 @@
 int main(void)
 /**                                                                               * main - Entry point                                                             *                                                                                * Return: Always 0 (Success)                                                     */
 {
-printf("\"with proper grammar, but the outcome is a piece of art,");
+printf("with proper grammar, but the outcome is a piece of art,");
 return (0);
 }
