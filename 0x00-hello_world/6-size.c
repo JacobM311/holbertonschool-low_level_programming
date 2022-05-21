0994c223aae6e3034c5b2faf34039fee5b5e3a3d
JacobM311 <jacobmarshall9608@yahoo.com>
2022-05-21 14:44:51 -0700

    commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,11 +1,8 @@
-#include<stdio.h>
+#include <stdio.h>
 
 int main(void) {
-    int x = 20;
-    char y = 'a';
-    //Using variable names as input
-    printf("Size of a char: %d\n", sizeof(int));
-    printf("Size of an int: %d\n", sizeof(char));
+    printf("Size of a char: %d\n", sizeof(char));
+    printf("Size of an int: %d\n", sizeof(int));
     printf("Size of a long int:: %d\n", sizeof(long int));
     printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
     printf("Size of a float: %d byte(s)\n", sizeof(float));
