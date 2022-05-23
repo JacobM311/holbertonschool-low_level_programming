d7da0eba4fe6aecd6618941b15c4819ad11877b4
JacobM311 <jacobmarshall9608@yahoo.com>
2022-05-23 13:55:13 -0700

    commit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int z;
+
+	for (z = 'a'; z <= 'z'; z++)
+	{
+		putchar(z);
+	}
+	return (0);
+}
