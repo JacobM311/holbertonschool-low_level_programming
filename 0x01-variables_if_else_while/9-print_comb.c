6218f50e4c7ef1d9c9253abb60f6639817edda5c
JacobM311 <jacobmarshall9608@yahoo.com>
2022-05-23 15:31:29 -0700

    commit

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int z;
+
+	for (z = '0'; z <= '9'; z++)
+	{
+		putchar(z);
+		putchar(',');
+		putchar(' ');
+	{
+		putchar('\n');
+	}
+	return (0);
+}
