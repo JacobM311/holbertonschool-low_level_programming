4f7a1b045e8720ebd1113e85d2dc4ea11c4d1eac
JacobM311 <jacobmarshall9608@yahoo.com>
2022-05-24 12:49:57 -0700

    commit

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,7 +5,7 @@
  *
  * Return: Always 0 (Success)
  */
-void print_alphabet(void);
+void print_alphabet(void)
 {
 	int z;
 
@@ -13,5 +13,5 @@ void print_alphabet(void);
 	{
 		_putchar(z);
 	}
-	putchar('\n');
+	_putchar('\n');
 }
