0805594768f9565b123b409bf9cdbfe365580017
JacobM311 <jacobmarshall9608@yahoo.com>
2022-05-24 11:51:50 -0700

    commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -10,22 +10,10 @@ int main(void)
 	int i;
 	
 	i = 0;
-	while(str[i] = '\0')
+	while(str[i] != '\0')
 	{
     		_putchar(str[i]);
     		i++;
 	}
+return(0)
 }
-/**0-putchar.c:13:8: error: suggest parentheses around assignment used as truth value [-Werror=parentheses]
-   13 |  while(str[i] = '\0');
-      |        ^~~
-0-putchar.c:13:2: error: this ‘while’ clause does not guard... [-Werror=misleading-indentation]
-   13 |  while(str[i] = '\0');
-      |  ^~~~~
-0-putchar.c:14:2: note: ...this statement, but the latter is misleadingly indented as if it were guarded by the ‘while’
-   14 |  {
-      |  ^
-0-putchar.c:18:1: error: control reaches end of non-void function [-Werror=return-type]
-   18 | }
-      | ^
-      */
