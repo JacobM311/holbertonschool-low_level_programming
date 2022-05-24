ada6a22fa0952142786f02ebea45a02856f8bd1f
JacobM311 <jacobmarshall9608@yahoo.com>
2022-05-24 14:31:40 -0700

    commit

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints
+ *
+ * Return: Always 0.
+ */
+void print_alphabet_x10(void)
+{
+	int x, y;
+	for (y = 0; y<= 9; y++)
+	{
+		for (x = 'a'; x <= 'z'; x++)
+		{
+			_putchar(x);
+		}
+		_putchar('\n');
+	}
+}
