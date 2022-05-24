ee7e267d83a556aa34bca21bc75a08cc7c88e67b
JacobM311 <jacobmarshall9608@yahoo.com>
2022-05-23 17:13:53 -0700

    commit

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,7 +9,7 @@ int main(void)
 {
 	int z;
 
-	for (z = '0'; z <= '10'; z++)
+	for (z = '0'; z <= '9'; z++)
 	{
 		putchar(z);
 	}
@@ -21,9 +21,6 @@ int main(void)
 	{
 		putchar(',');
 		putchar(' ');
-	}
-	{
-		putchar('\n');
 	}
 
 	return (0);
