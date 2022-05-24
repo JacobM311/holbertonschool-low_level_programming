3539eaca3118231f833e6da47abac16a48cc4848
JacobM311 <jacobmarshall9608@yahoo.com>
2022-05-24 15:35:52 -0700

    commit

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -8,12 +8,15 @@ int _isalpha(int c)
 {
 if (c >= 'a' && c <= 'z')
 {
-	return (1)
+	return (1);
 }
 else if
-		if (c >= 'A' && c <= 'Z')
-{
+{		
+	if (c >= 'A' && c <= 'Z')
+	{
+
 		return (1);
+	}
 }
 else
 {
