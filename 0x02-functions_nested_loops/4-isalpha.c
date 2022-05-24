bce30d7470da50db2b2c83aff1726419d45e1461
JacobM311 <jacobmarshall9608@yahoo.com>
2022-05-24 15:19:28 -0700

    commit

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _isalpha - something
+ * @c: hi
+ * Return: Always 0.
+ */
+int _isalpha(int c);
+{
+if (c >= 'a' && c <= 'z')
+	{
+		if (c >= 'A' && c <= 'Z')
+		return (1)
+	}
+return (0)
+}
