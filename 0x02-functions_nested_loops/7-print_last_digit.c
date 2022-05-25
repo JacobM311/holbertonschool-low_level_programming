6699998c0a996aa3884a177af2e8ab792737f2cf
JacobM311 <jacobmarshall9608@yahoo.com>
2022-05-24 17:24:42 -0700

    commit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_last_digit - isdf
+ * @j: variable
+ * Return: last digit
+ */
+int print_last_digit(int j)
+{
+j = j % 10;
+if (j > 0)
+	j = j * -1
+_putchar(j + 48);
+return (j);
+}
+
