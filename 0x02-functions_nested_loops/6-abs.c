d87b46f1f2ef1ad83795f3a556221a610748e588
JacobM311 <jacobmarshall9608@yahoo.com>
2022-05-24 20:56:19 -0700

    commit

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,11 +1,17 @@
 #include "main.h"
 /**
- * int _abs - ryn
- * @n: yes
- * return: always 0.
+ * _abs - ryn
+ * @x: yes
+ * Return: abs
  */
 int _abs(int x)
 {
-	x = _abs(x);
+int r;
+	r = _abs(x);
 	return (0);
 }
+/**
+ * No description found for parameter or member 'x'
+6-abs.c:8: warning: Excess function parameter 'r' description in '_abs'
+6-abs.c:7: warning: No description found for return value of '_abs'
+*/
