79c90ce80e637d88215be9d99157e35ca7d21dd8
JacobM311 <jacobmarshall9608@yahoo.com>
2022-05-24 21:55:00 -0700

    commit

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,12 +1,31 @@
 #include "main.h"
 /**
  * add - ryn
- * @x: yes
- * @y: yes
+ * @a: yes
+ * @b: yes
  * Return: Always 0.
  */
-int add(int x, int y)
+int add(int a, int b)
 {
-	_putchar((y + x) + '0');
-	return (0);
+	int add(int a, int b)
+{
+    // for loop will start from 1 and move till the value of
+    // second number , first number(a) is incremented in for
+    // loop
+    for (int i = 1; i <= b; i++)
+        a++;
+    return (a);
+}
+ 
+int main()
+{
+    // first number is 10 and second number is 32 , for loop
+    // will start from 1 and move till 32 and the value of a
+    // is incremented 32 times which will give us the total
+    // sum of two numbers
+ 
+    int a = add(10, 32);
+    cout << a;
+    return (0);
+}
 }
