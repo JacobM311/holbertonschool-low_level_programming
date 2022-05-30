3e6ea2abcca03ce2d98bffde3e3154733e875e0b
JacobM311 <jacobmarshall9608@yahoo.com>
2022-05-30 01:26:49 -0700

    commit

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,14 +1,21 @@
 #include "main.h"
 
+/*
+ * print_square: print square.
+ *
+ * @size: yes
+ */
+
 void print_square(int size)
+
 {
 int i, j;
-for (i = 1; i <= size; i++)
-{
-	for (j = 1; j <= size; j++)
+	for (i = 1; i <= size; i++)
+	{
+		for (j = 1; j <= size; j++)
 			{
 			_putchar('#');
 			}
-	_putchar('\n')
-}
+		_putchar('\n');
+	}
 }
