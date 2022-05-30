98d7bca4e80641b49bc77a9991eabfd4c3f41767
JacobM311 <jacobmarshall9608@yahoo.com>
2022-05-29 21:03:03 -0700

    commit

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+void print_diagonal(int n)
+{
+int x, i, j;
+i = 0;
+j = 0;
+x = '\\';
+if (n > 0)
+{
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < n; j++)
+	{
+		if (j == i)
+		_putchar(x);
+		else
+		_putchar(' ');
+	}
+	_putchar('\n');
+	}
+}
+if (n <= 0)
+	{
+		_putchar('\n');
+	}
+}
