ce6cb868f77a606b8150e86d96eeee49ce8d2322
JacobM311 <jacobmarshall9608@yahoo.com>
2022-05-30 02:22:45 -0700

    commit

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,7 +10,11 @@ int main(void)
 int x;
 	for  (x = 1; x <= 100; x++)
 	{
-		if (x % 15 == 0)
+		if (x == 100)
+		{
+			printf("Buzz");
+		}
+		else if (x % 15 == 0)
 		{
 			printf("FizzBuzz ");
 		}
@@ -21,10 +25,6 @@ int x;
 		else if (x % 3 == 0)
 		{
 			printf("Fizz ");
-		}
-		else if (x == 100)
-		{
-			printf("Buzz");
 		}
 		else
 		{
