5f6780805eaedd3f1471d3e02b9dcd367670ed6e
JacobM311 <jacobmarshall9608@yahoo.com>
2022-05-30 01:16:14 -0700

    commit

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -5,30 +5,22 @@
  * @n: yes
  */
 
-void print_diagonal(int n)
+void print_diagonal (int n)
 {
 
-int i, j;
-i = 0;
-j = 0;
+int j;
 
 	if (n > 0)
 	{
-		for (i = 0; i < n; i++)
+		for (j = 0; j <= n; j++)
 		{
-			for (j = 0; j < n; j++)
-			{
-				if (j == i)
+				while (--j)
+				{
+					_putchar(' ');
 				_putchar('\\');
-				else
-				_putchar(' ');
-			}
-		_putchar('\n');
+				_putchar('\n');
+				}
 		}
 
 	}
-	else if (n <= 0)
-		{
-		_putchar('\n');
-		}
 }
---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+void print_square(int size)
+{
+int i, j;
+for (i = 1; i <= size; i++)
+{
+	for (j = 1; j <= size; j++)
+			{
+			_putchar('#');
+			}
+	_putchar('\n')
+}
+}
