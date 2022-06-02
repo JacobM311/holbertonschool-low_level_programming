c41b1708aaf26b946e7c49bb9626eb0d6bc54c22
JacobM311 <jacobmarshall9608@yahoo.com>
2022-06-02 11:33:39 -0700

    commit

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -9,15 +9,20 @@
 void print_array(int *a, int n)
 {
 	int x = 0;
-if (n >= 0)
+if (n > 0)
 {
 	for (x = 0; x < n - 1; x++)
 	{
 		printf("%d ,", a[x]);
 	}
-	for (x = n - 1; n < x; x++)
+	for (x = n - 1; x < n; x++)
 	{
 		printf("%d\n", a[x]);
 	}
+
+}
+else
+{
+	printf("\n");
 }
 }
