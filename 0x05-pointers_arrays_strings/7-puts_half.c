d9edf1b20db284f95f0a684d138e135ae44e5e8a
JacobM311 <jacobmarshall9608@yahoo.com>
2022-06-02 10:01:45 -0700

    commit

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -21,7 +21,6 @@ void puts_half(char *str)
 		{
 			_putchar(str[i]);
 		}
-		_putchar('\n');
 	}
 	else
 	{
@@ -29,6 +28,6 @@ void puts_half(char *str)
 		{
 			_putchar(str[i]);
 		}
-		_putchar('\n');
 	}
+_putchar('\n');
 }
