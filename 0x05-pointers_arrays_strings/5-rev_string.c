980b6251e274717688c8e224bc3c217fcebbecbc
JacobM311 <jacobmarshall9608@yahoo.com>
2022-06-02 13:51:46 -0700

    commit

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,3 +1,19 @@
 #include "main.h"
 
 void rev_string(char *s)
+{
+	int b;
+	int c = 0;
+	int e;
+
+	while (s[c] != '\0')
+	{
+		c++;
+	}
+	for (b = 0; b < c / 2; b++)
+	{
+		e = s[b];
+		s[b] = s[c - b -1]
+		s[c - b -1] = e;
+	}
+}
