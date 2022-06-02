6a21816783c2d631a15b3cbac3c7038d32fd7261
JacobM311 <jacobmarshall9608@yahoo.com>
2022-06-02 13:04:29 -0700

    commit

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -5,12 +5,16 @@
  * @s: yes
  */
 
-void print_rev(char *s)
+void print_rev(char *s, char *b)
 {
 	if (*s != '\0')
 	{
-		print_rev(s + 1);
+		print_rev(s + 1, b);
 		_putchar(*s);
+		if (s == b)
+		{
+			_putchar('\n');
+		}
 	}
 	
 }
