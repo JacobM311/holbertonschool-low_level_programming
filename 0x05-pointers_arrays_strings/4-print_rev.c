d087a305acccb0d51776183cab9eefbc46ef8c24
JacobM311 <jacobmarshall9608@yahoo.com>
2022-06-02 09:25:57 -0700

    commit

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -12,8 +12,5 @@ void print_rev(char *s)
 		print_rev(s + 1);
 		_putchar(*s);
 	}
-	else
-	{
-		_putchar('\n');
-	}
+	
 }
--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,29 +1,32 @@
 #include "main.h"
 
+/**
+ * puts_half- puts half
+ * @str: yes
+ */
+
 void puts_half(char *str)
 {
 	int len = 0;
 	int i = 0;
-	int len1 = 0;
 
 	while (*str != '\0')
 	{
-		str++;
 		len++;
 	}
-	while (*str != 0)
-	{
-		str--;
-	}
-	len1 = len / 2;
 
-	while (str[i] != '\0')
+	if (len % 2 == 0)
 	{
-		if (i >= len1)
+		for (i = len / 2; i <= len; i++)
 		{
 			_putchar (str[i]);
 		}
-	i++;
 	}
-
+	if (len % 2 != 0)
+	{
+		for (i = len / 2 + 1; i <= len; i++)
+		{
+			_putchar (str[i]);
+		}
+	}
 }
