9c62f3d76d98b25b4f328073a360ea00bf5ad69a
JacobM311 <jacobmarshall9608@yahoo.com>
2022-06-06 15:06:18 -0700

    commit

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+
+
+char *string_toupper(char *c)
+
+{
+	int i;
+
+	for (i = 0; c[i] != '\0'; i++)
+	{
+		if (c[i] <= 'z' && c[i] >= 'a')
+		{
+			c[i] = c[i] - 32;
+		}
+	}
+	return (c);
+}
