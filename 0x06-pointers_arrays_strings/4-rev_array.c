04122fd6871b0f66af57cd06ff2a96e65d4feb70
JacobM311 <jacobmarshall9608@yahoo.com>
2022-06-06 14:42:06 -0700

    commit

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * reverse_array-yes
+ * @a: yes
+ * @n: yes
+ */
+
+void reverse_array(int *a, int n)
+{
+	int i;
+
+	n--;
+
+	for (i = 0; i < n \ 2; i++)
+	{
+		temp = a[i];
+		a[i] = a[n - i];
+		a[n - i] = temp;
+	}
+}
