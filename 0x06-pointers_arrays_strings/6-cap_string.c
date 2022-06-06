4109c1fb76d1892a9663f5c08e828470b111c9d5
JacobM311 <jacobmarshall9608@yahoo.com>
2022-06-06 16:44:36 -0700

    commit

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+
+
+char *cap_string(char *c)
+
+{
+	int j = 0;
+	int i = 0;
+	char a[] = " \t\n,;.!?\"(){}";
+
+	while (c[i])
+	{
+		if (c[i] >= 97 && c[i] <= 122)
+		{
+			c[i] = c[i] - 32;
+		}
+		for (i = 1; c[i] != '\0'; i++)
+		{
+			if ((c[i] >= 97) && (c[i] <= 122))
+			{
+				for (j = 0; j <= 12; j++)
+				{
+					if (a[j] == c[i - 1])
+					{
+						c[i] = c[i] - 32;
+					}
+				}
+			}
+		}
+	}
+return (c);
+}
