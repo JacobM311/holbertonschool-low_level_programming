e91da4a867db540e05d123736e0ffd8e27c90b1c
JacobM311 <jacobmarshall9608@yahoo.com>
2022-06-06 11:33:20 -0700

    commit

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,12 +1,21 @@
 #include "holberton.h"
 
+/**
+ * *_strcat- yes
+ * @dest: yes
+ * @src: yes
+ * Return: yes
+ */
+
 char *_strcat(char *dest, char *src)
 
 {
 int i;
 int j;
 
-for (i = 0; dest[i] != '\0'; i++);
+for (i = 0; dest[i] != '\0'; i++)
+{
+}
 
 for (j = 0; src[j] != '\0'; j++)
 
