6299248d78c0ca7726414a6effa21502b4b1e54c
JacobM311 <jacobmarshall9608@yahoo.com>
2022-06-07 14:28:01 -0700

    commit

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * int_strspn-yes
+ * @s: yes
+ * @accept: yes
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	int count = 0;
+
+	while (accept[i])
+	{
+		j = 0;
+		while (s[j] != 32)
+		{
+			if (accept[i] = s[j])
+			{
+				count++;
+			}
+			j++;
+		}
+		i++;
+	}
+return (count);
+}
