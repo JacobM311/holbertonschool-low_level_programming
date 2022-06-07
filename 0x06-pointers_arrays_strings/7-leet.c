1e24ab20e22aefc7fbfdc07ad91612e41c83fc3c
JacobM311 <jacobmarshall9608@yahoo.com>
2022-06-06 19:14:25 -0700

    commit

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -3,16 +3,16 @@
 /**
  * leet-yes
  * @c: yes
+ * Return: yes
  */
 
 char *leet(char *c)
-
 {
 	char duggystyle[] = "aAeEoOtTlL4433007711";
 	int i = 0;
 	int j = 0;
 
-	for (i = 0; i != '\0'; i++)
+	for (i = 0; c[i] != '\0'; i++)
 	{
 		for (j = 0; j <= 9; j++)
 		{
