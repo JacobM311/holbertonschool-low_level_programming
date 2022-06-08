df9120fc31c59cf511de7cf8b9e0eb20ac6d5dc6
JacobM311 <jacobmarshall9608@yahoo.com>
2022-06-08 14:57:14 -0700

    recursion

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * @s: yes
+ * _print_rev_recursion-yes
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return;
+	}
+	_print_rev_recursion(s + 1);
+	_putchar(*s);
+}
+
