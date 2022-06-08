9c18b324712e5de41dac31950e5c2c39ec8a133a
JacobM311 <jacobmarshall9608@yahoo.com>
2022-06-08 14:00:40 -0700

    commit

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,13 +1,12 @@
 #include "main.h"
+#include <stdio.h>
 
 void _puts_recursion(char *s)
 {
 	if (*s == '\0')
-	{
-	}
-		return;
+	{}
+		return ;
 		_putchar(*s);
 		_puts_recursion(s + 1);
-	_putchar('\n');
 }
 
