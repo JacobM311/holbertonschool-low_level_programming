23e96592284416ae170efc0c9308d55e4499562c
JacobM311 <jacobmarshall9608@yahoo.com>
2022-06-08 15:27:19 -0700

    recursion

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -19,3 +19,10 @@ void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
 void _puts_recursion(char *s);
+void _print_rev_recursion(char *s);
+int _strlen_recursion(char *s);
+int factorial(int n);
+int _pow_recursion(int x, int y);
+int _sqrt_recursion(int n);
+int is_prime_number(int n);
+
