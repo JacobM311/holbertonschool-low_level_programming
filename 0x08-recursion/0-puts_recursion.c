8dd8bdce978909c0f6375923cfd5021b17980e0e
JacobM311 <jacobmarshall9608@yahoo.com>
2022-06-08 14:18:45 -0700

    recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -4,8 +4,11 @@
 void _puts_recursion(char *s)
 {
 	if (*s == '\0')
-		return ;
-		_putchar(*s);
-		_puts_recursion(s + 1);
+	{
+		_putchar('\n');
+		return;
+	}
+	_putchar(*s);
+	_puts_recursion(++s);
 }
 
