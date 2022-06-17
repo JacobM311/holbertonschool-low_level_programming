65ef673a07bfadf79ca2d6d1d8684aa91d84c0fa
JacobM311 <jacobmarshall9608@yahoo.com>
2022-06-16 21:32:14 -0700

    commit

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -2,41 +2,16 @@
 #include <stdlib.h>
 #include "main.h"
 /**
- *alloc_grid - prints a grid of integers
+ *free_grid - prints a grid of integers
  *@width: width of the grid
  *@height: height of the grid
  *Return: nothing else
  */
-int **alloc_grid(int width, int height)
+void free_grid(int **grid, int height)
 {
-	int **array;
 	int i;
-
-	array = (int **)malloc(height * sizeof(int *));
-
-	if (height <= 0 || width <= 0)
-	{
-		return (NULL);
-	}
-	if (array == 0)
-	{
-		free(array);
-		return (NULL);
-	}
-	for (i = 0; i < height; i++)
-	{
-	array[i] = (int *)malloc(width * sizeof(int));
-		if (array[i] == NULL)
-		{
-			for (i = 0; i < height; i++)
-			{
-				free(array[i]);
-			}
-			free(array);
-			return (NULL);
-		}
-	}
-
-return (array);
-
+	
+	for (i = 0; x < height; i++)
+		free(grid[x]);
+	free(grid);
 }
