9bb62d5d19eaaeb11caf5ac1f132c7988fcb1953
JacobM311 <jacobmarshall9608@yahoo.com>
2022-06-16 20:00:49 -0700

    commit

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+int **alloc_grid(int width, int height)
+{
+	int **array;
+	int i;
+
+	array = (int **)malloc(height * sizeof(int *));
+
+	if (height <= 0 || width <= 0)
+	{
+		return (NULL);
+	}
+	if (array == 0)
+	{
+		free(array);
+		return (NULL);
+	}
+	for (i = 0; i < height; i++)
+	{
+	array[i] = (int *)malloc(width * sizeof(int));
+		if (array[i] == NULL)
+		{
+			for (i = 0; i < height; i++)
+			{
+				free(array[i]);
+			}
+			free(array);
+			return (NULL);
+		}
+	}
+return (array);
+}
