0bd931e604af2d87df8988a804a08171cff4aed1
JacobM311 <jacobmarshall9608@yahoo.com>
2022-06-16 19:15:29 -0700

    commit

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * *str_concat - concatenates 2 strings
+ * @s1: string
+ * @s2: string
+ * Return: concatenated string.
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	char *a;
+	unsigned int i;
+
+	a = malloc(sizeof(char) * (strlen(s1) + strlen(s2) + 1));
+
+	if (a == NULL)
+		return (NULL);
+	while (*s1)
+	{
+		a[i] = *s1;
+		i++;
+		s1++;
+	}
+
+	 while (*s2)
+        {
+                a[i] = *s2;
+                i++;
+                s2++;
+        }
+return (a);
+}
