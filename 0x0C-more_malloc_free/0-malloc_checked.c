823e63cfb2560acd45a2a393a5a4fb44ff42219f
JacobM311 <jacobmarshall9608@yahoo.com>
2022-06-21 09:17:06 -0700

    commit

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * *malloc_checked - runs malloc
+ * @b: int being allocated
+ * Return: allocated memory
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	int *p;
+
+	p = malloc(b);
+
+	if (p == NULL)
+		exit (98);
+	return (p);
+}
