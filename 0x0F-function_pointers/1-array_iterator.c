a69b144354a3af5e75c9dc3d1944aa8bbb6be36e
JacobM311 <jacobmarshall9608@yahoo.com>
2022-06-23 17:13:02 -0700

    commit

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "function_pointers.h"
+
+/**
+ * array_iterator - iteratores through an array
+ * @array: passed pointer to an array
+ * @size: size of array
+ * @action: pointer to function being used
+ */
+
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i = 0;
+
+	if (array != NULL && action != NULL)
+	{
+		while (i < size)
+		{
+			action(array[i]);
+		}
+		i++;
+	}
+}
