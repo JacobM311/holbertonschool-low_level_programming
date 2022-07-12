674c25ae07694ef6f23e120e4ee28266aa7ee1e8
JacobM311 <jacobmarshall9608@yahoo.com>
2022-07-12 15:12:24 -0700

    commit

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -1,5 +1,12 @@
 #include "lists.h"
 
+/**
+ * add_nodeint_end - adds a new node to end of lisint_t list
+ * @head: pointer to head of list
+ * @n: int to be added to node
+ * Return: address of new node or NULL if failed.
+ */
+
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
 	listint_t *newnode, *lastnode;
