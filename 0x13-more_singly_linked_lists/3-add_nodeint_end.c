482a53a0801169cf46acf135b9ee8701e74695c4
JacobM311 <jacobmarshall9608@yahoo.com>
2022-07-12 15:08:47 -0700

    commit

---
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+	listint_t *newnode, *lastnode;
+
+	newnode = malloc(sizeof(listint_t));
+		if (newnode == NULL)
+			return (NULL);
+
+	newnode->n = n;
+	newnode->next = NULL;
+
+	if (*head == NULL)
+		*head = newnode;
+	else
+	{
+		lastnode = *head;
+		while (lastnode->next != NULL)
+			lastnode = lastnode->next;
+		lastnode->next = newnode;
+	}
+	return(*head);
+}
