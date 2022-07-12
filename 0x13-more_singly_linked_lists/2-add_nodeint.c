8ddd50231cc7db2e1b5b03b2b839981309ad3946
JacobM311 <jacobmarshall9608@yahoo.com>
2022-07-12 13:56:10 -0700

    commit

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -17,8 +17,8 @@ listint_t *add_nodeint(listint_t **head, const int n)
 		return (NULL);
 
 	newnode->n = n;
-	newnode->next = *head
-	
+	newnode->next = *head;
+
 	*head = newnode;
 
 	return (newnode);
