752166120a7c476182485bdb35f66b1bf4b78229
JacobM311 <jacobmarshall9608@yahoo.com>
2022-07-13 17:42:16 -0700

    commit

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -10,7 +10,7 @@ unsigned int binary_to_uint(const char *b)
 
 	for (i = 0; b[i] != '\0'; i++)
 	{
-		if (b[i] != '0' || b[i] != '1' || ((b[i]) == '\0'))
+		if (b[i] != '0' || b[i] != '1' || b[i] == '\0')
 		{
 			return (0);
 		}
