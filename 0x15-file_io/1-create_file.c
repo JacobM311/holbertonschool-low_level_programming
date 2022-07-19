da048efa6cb55375b6281a35081b808995456125
JacobM311 <jacobmarshall9608@yahoo.com>
2022-07-18 21:25:46 -0700

    commit

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,26 +9,29 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int fd, fw, length;
+	int fo, fw, length;
 
 	if (filename == NULL)
 		return (-1);
 	if (text_content == NULL)
-		text_content = " ";
+		text_content = "";
 
-	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	fo = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 
-	if (fd == -1)
+
+	if (fo == -1)
 		return (-1);
 
 	length = _strlen(text_content);
 
-	fw = write(fd, text_content, length);
+	fw = write(fo, text_content, length);
 
-	if (fd == -1 || fw == -1)
+	if (fo == -1 || fw == -1)
 		return (-1);
 
-	close(fd);
+
+
+	close(fo);
 	return (1);
 }
 
