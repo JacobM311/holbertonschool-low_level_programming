927c8d625609d70f4b043a13bee48e23ec0f9b65
JacobM311 <jacobmarshall9608@yahoo.com>
2022-08-02 18:09:19 -0700

    commit

--- 0x18-doubly_linked_lists/0-print_dlistint.c
+++ 0x18-doubly_linked_lists/0-print_dlistint.c
@@ -1,6 +1,12 @@
 #include "lists.h"
 #include <stdio.h>
 
+/**
+ * print_dlistint - prints dlistint
+ * @h: pointer to head
+ * Return: number of nodes
+ */
+
 
 size_t print_dlistint(const dlistint_t *h)
 {
